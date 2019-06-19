0876b6fe08a5138fad186f04892092021144305b
Anton Lo <antshot19@gmail.com>
2019-06-19 23:42:06 +0300

    Second task of java second course.Exeptions.

---
+++ src/lesson2/Main.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] array = {{"1","1","1","1"},
+                            {"1","1","1","1"},
+                            {"1","1","1","1"},
+                            {"1","1","1","1"}};
+
+    }
+
+    public static  void summArray(String[][] array) {
+        try {array[0].length != 4 || array.length != 4;
+        } catch ()
+
+    }
+}
