9e5c917f76026febea9ed3ac9e7b3aec0a195dbc
Anton Lo <antshot19@gmail.com>
2019-06-20 00:14:30 +0300

    Second task of java second course.Exeptions.

--- src/lesson2/Main.java
+++ src/lesson2/Main.java
@@ -7,11 +7,13 @@ public class Main {
                             {"1","1","1","1"},
                             {"1","1","1","1"}};
 
+
+        summArray(array);
     }
 
-    public static  void summArray(String[][] array) {
-        try {array[0].length != 4 || array.length != 4;
-        } catch ()
-
+    public static  void summArray(String[][] array) throws MyArraySizeException{
+        if (array[0].length != 4 || array.length != 4) {
+            throw new MyArraySizeException("Заданный двумерный строковый массив размером не соответствует размеру 4х4", array);
+        }
     }
 }
