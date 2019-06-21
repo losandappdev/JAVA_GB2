7731cb7f2e989c3a9d816e4551c3633a77cf9e15
Anton Lo <antshot19@gmail.com>
2019-06-21 03:49:02 +0300

    Third task of java second course.Collections.

--- src/lesson3/Main.java
+++ src/lesson3/Main.java
@@ -2,25 +2,53 @@ package lesson3;
 
 import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
 
+import java.security.PublicKey;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.Random;
 
 import javax.naming.Name;
 
+import lesson1.marathon.obstacles.Course;
+
 public class Main {
     public static void main(String[] args) {
-        Random rnd = new Random();
-        String[] arr = new String[]{"Друзья","Игра","Радость","Успех","Деньги","Зависимость",
+//        Random rnd = new Random();
+        String[] arr = new String[]{"Друзья","Игра","Радость","Успех","Деньги","Погода",
                 "Конфетки","Телефон","Дела","Семья","Привычка","Судьба","Ручка","Взгляд","Кровать","Выбор",
-                "Институт","Мечты","Память","Смех","Улыбка","Глаза","Взгляд","Жизнь"};
+                "Институт","Мечты","Смех","Улыбка","Глаза","Взгляд","Смех"};
+
+        uniqueWords(arr);
+
+//        System.out.println(words);
 
 
-        ArrayList<String>  words = new ArrayList<>();
-        Collections.addAll(words, arr);
-        System.out.println(words);
     }
 
+    public static void uniqueWords(String[] arr){
+        ArrayList<String>  words = new ArrayList<>();
+        Collections.addAll(words, arr);
 
+        int count;
+//        Iterator<String> wordsIterator = words.iterator();
+
+        for (int i = words.size() - 2; i > 0; i--) {
+            count = 1;
+//            System.out.println(words.get(i));
+            for (int j = i-1; j > -1; j--) {
+//                System.out.println(words.get(i)+ "  " + words.get(j));
+                if (words.get(i).equals(words.get(j))){
+
+//                    System.out.println(words.get(i)+ "  " + words.get(j));
+//                    words.remove(j);
+                    count +=1;
+
+                }
+            }
+//            words.remove(i);
+            System.out.println(words.get(i) + "  " + count);
+        }
+    }
 }
