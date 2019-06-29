6e188c61bb59c108bcef5fe37b7a8813cd41ef50
Anton Lo <antshot19@gmail.com>
2019-06-29 19:14:23 +0300

    Five task of java second course.Threds.

--- src/lesson5/Main.java
+++ src/lesson5/Main.java
@@ -1,4 +1,88 @@
 package lesson5;
 
+import com.sun.rowset.FilteredRowSetImpl;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        firstArray();
+        secondArray();
+    }
+
+
+    /**
+     * Выполненние вычисление в один поток
+     */
+    public static void firstArray() {
+        final int size = 10000000;
+        float[] arr = new float[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+
+        long a = System.currentTimeMillis();
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+        }
+
+        System.currentTimeMillis();
+        System.out.println(" Время выполнения в один поток " + (System.currentTimeMillis() - a));
+    }
+
+    /**
+     * Выполненние вычисление в два потока
+     */
+    public static void secondArray() {
+        final int size = 10000000;
+        final int h = size / 2;
+        float[] arr = new float[size];
+        float[] a1 = new float[h];
+        float[] a2 = new float[h];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = 1;
+        }
+
+        long a = System.currentTimeMillis();
+
+        System.arraycopy(arr, 0, a1, 0, h);
+        System.arraycopy(arr, h, a2, 0, h);
+
+        long b = System.currentTimeMillis();
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                a1[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < h; i++) {
+                a2[i] = (float) (arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+            }
+        });
+        long x = System.currentTimeMillis();
+        t1.start();
+        long y = System.currentTimeMillis();
+        t2.start();
+        long z = System.currentTimeMillis();
+
+        long c = System.currentTimeMillis();
+        System.arraycopy(a1, 0, arr, 0, h);
+        System.arraycopy(a2, 0, arr, h, h);
+        long d = System.currentTimeMillis();
+
+//        for (int i = 0; i < h; i++) {
+//            arr[i] = (float)(arr[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
+//        }
+
+        System.currentTimeMillis();
+        System.out.println(" Время выполнения в два потока \n(время разбивки массива на 2, просчета каждого из двух массивов и склейки.) " + (b - a) + (y - x) + (z - y) + (d - c));
+        System.out.println(" Время выполнения в два потока " + (System.currentTimeMillis() - a));
+    }
+
 }
